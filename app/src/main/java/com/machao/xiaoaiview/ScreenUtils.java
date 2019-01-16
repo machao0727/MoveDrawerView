4342f4bea2921ccefb16a9f26067ca65a80345b0
machao <myemail>
2019-01-16 17:29:43 +0800

    first commit

---
+++ app/src/main/java/com/machao/xiaoaiview/ScreenUtils.java
@@ -0,0 +1,156 @@
+package com.machao.xiaoaiview;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Build;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.ViewConfiguration;
+import android.view.WindowManager;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by chao on 2016/10/25.
+ * 屏幕宽高工具类
+ */
+
+public class ScreenUtils {
+    private static int screenW;
+    private static int screenH;
+    private static int virtualH;
+    private static float screenDensity;
+    private static int statusBarHeight = 0;
+
+    public static int getVirtualH() {
+        return virtualH;
+    }
+
+    public static void initScreen(Context mActivity) {
+        WindowManager wm = (WindowManager) mActivity.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(outMetrics);
+        screenW = outMetrics.widthPixels;
+        screenH = outMetrics.heightPixels;
+        screenDensity = outMetrics.density;
+        getStatusBarHeight(mActivity);
+        virtualH = getNavigationBarHeight(mActivity);
+    }
+
+    private static void getStatusBarHeight(Context context) {
+        Resources res = context.getResources();
+        int resourceId = res.getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            statusBarHeight = res.getDimensionPixelSize(resourceId);
+        }
+    }
+
+    //获取虚拟按键的高度
+    public static int getNavigationBarHeight(Context context) {
+        int result = 0;
+        if (hasNavBar(context)) {
+            Resources res = context.getResources();
+            int resourceId = res.getIdentifier("navigation_bar_height", "dimen", "android");
+            if (resourceId > 0) {
+                result = res.getDimensionPixelSize(resourceId);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 检查是否存在虚拟按键栏
+     *
+     * @param context
+     * @return
+     */
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    public static boolean hasNavBar(Context context) {
+        Resources res = context.getResources();
+        int resourceId = res.getIdentifier("config_showNavigationBar", "bool", "android");
+        if (resourceId != 0) {
+            boolean hasNav = res.getBoolean(resourceId);
+            // check override flag
+            String sNavBarOverride = getNavBarOverride();
+            if ("1".equals(sNavBarOverride)) {
+                hasNav = false;
+            } else if ("0".equals(sNavBarOverride)) {
+                hasNav = true;
+            }
+            return hasNav;
+        } else { // fallback
+            return !ViewConfiguration.get(context).hasPermanentMenuKey();
+        }
+    }
+
+    /**
+     * 判断虚拟按键栏是否重写
+     *
+     * @return
+     */
+    private static String getNavBarOverride() {
+        String sNavBarOverride = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            try {
+                Class c = Class.forName("android.os.SystemProperties");
+                Method m = c.getDeclaredMethod("get", String.class);
+                m.setAccessible(true);
+                sNavBarOverride = (String) m.invoke(null, "qemu.hw.mainkeys");
+            } catch (Throwable e) {
+            }
+        }
+        return sNavBarOverride;
+    }
+
+
+    public static int getDpi(Context context) {
+        int dpi = 0;
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = windowManager.getDefaultDisplay();
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        @SuppressWarnings("rawtypes")
+        Class c;
+        try {
+            c = Class.forName("android.view.Display");
+            @SuppressWarnings("unchecked")
+            Method method = c.getMethod("getRealMetrics", DisplayMetrics.class);
+            method.invoke(display, displayMetrics);
+            dpi = displayMetrics.heightPixels;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return dpi;
+    }
+
+    public static int getScreenW() {
+        return screenW;
+    }
+
+    public static int getScreenH() {
+        return screenH;
+    }
+
+    public static float getScreenDensity() {
+        return screenDensity;
+    }
+
+    public static int getStatusBarHeight() {
+        return statusBarHeight;
+    }
+
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    public static int dp2px(float dpValue) {
+        return (int) (dpValue * getScreenDensity() + 0.5f);
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    public static int px2dp(float pxValue) {
+        return (int) (pxValue / getScreenDensity() + 0.5f);
+    }
+}
---
+++ app/src/main/java/com/machao/xiaoaiview/XiaoAiView.java
@@ -0,0 +1,236 @@
+package com.machao.xiaoaiview;
+
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+
+/**
+ * create by：mc on 2019/1/11 14:16
+ * email:
+ * 小艾View
+ */
+public class XiaoAiView extends View {
+
+    private Context context;
+
+    private int ScreenW;
+
+    private float startX;
+    private float startY;
+    private float endX;
+    private float endY;
+    private AnimatorSet set;
+    private int startSizeW;
+
+
+    private final RectF roundRect = new RectF();
+    private final Paint zonePaint = new Paint();
+    private final Paint textPaint = new Paint();
+    private Bitmap bitmap;
+    private int bitmapW;
+    private int bitmapH;
+    private int baseLineY;
+    private float currentX;
+    private float currentY;
+    private float currentW;
+    private int radius;
+    private int sizeH;//默认高度，高度默认保持不变
+    private int endSizeW;//结束宽度，与高度相同，为正方形
+
+    private int paddingSize = 15;//内容里边距
+
+    private String text = "默认文本";
+
+    private Bitmap bitmapDisplay = null;
+    private Matrix matrix = null;
+    private boolean transComplete = false;//移动完成
+    private int during = 1000;
+
+    public XiaoAiView(Context context) {
+        super(context);
+        init(context, null, 0);
+    }
+
+    public XiaoAiView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs, 0);
+    }
+
+    public XiaoAiView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs, defStyleAttr);
+    }
+
+    private void init(Context context, AttributeSet attrs, int defStyleAttr) {
+        this.context = context;
+        ScreenUtils.initScreen(context);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.XiaoAiView, defStyleAttr, 0);
+        int backgroundColor = a.getColor(R.styleable.XiaoAiView_backgroundColor, Color.GREEN);
+        int icon = a.getResourceId(R.styleable.XiaoAiView_icon, -1);
+        int textSize = a.getDimensionPixelSize(R.styleable.XiaoAiView_textSize, 40);
+        text = a.getString(R.styleable.XiaoAiView_text);
+        int margin = a.getDimensionPixelOffset(R.styleable.XiaoAiView_margin, 100);
+        paddingSize = a.getDimensionPixelSize(R.styleable.XiaoAiView_padding, 15);
+        during = a.getInteger(R.styleable.XiaoAiView_during, 1000);
+        a.recycle();
+
+        int screenH = ScreenUtils.getScreenH();
+        ScreenW = ScreenUtils.getScreenW();
+        zonePaint.setAntiAlias(true);
+        zonePaint.setColor(backgroundColor);
+        zonePaint.setStyle(Paint.Style.FILL);
+        bitmap = BitmapFactory.decodeResource(getResources(), icon);
+        bitmapDisplay = bitmap;
+        matrix = new Matrix();
+
+        bitmapH = bitmap.getHeight();
+        bitmapW = bitmap.getWidth();
+
+        sizeH = (bitmapH > bitmapW ? bitmapH : bitmapW) + paddingSize * 2;
+
+        endSizeW = sizeH;
+        radius = sizeH / 2;
+
+        textPaint.setColor(Color.WHITE);
+        textPaint.setStyle(Paint.Style.FILL);
+        textPaint.setStrokeWidth(2);
+        textPaint.setTextSize(textSize);
+
+        //=============默认设置==========//
+        startSizeW = (int) (ScreenW * 0.8);
+        startX = (float) ((ScreenW * 0.2) / 2);
+        startY = (float) screenH /2;
+        endX = ScreenW - margin - sizeH;
+        endY = screenH - ScreenUtils.getVirtualH() * 3 - margin;
+        currentX = startX;
+        currentY = startY;
+        currentW = startSizeW;
+        //=============默认设置==========//
+
+
+        Paint.FontMetrics fontMetrics = textPaint.getFontMetrics();
+        float top = fontMetrics.top;//为基线到字体上边框的距离,即上图中的top
+        float bottom = fontMetrics.bottom;//为基线到字体下边框的距离,即上图中的bottom
+        baseLineY = (int) (sizeH / 2 - top / 2 - bottom / 2);
+    }
+
+
+    /**
+     * 开始动画
+     */
+    public void startAnimation() {
+        set.start();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        setMeasuredDimension((int) currentW, sizeH);
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (!transComplete) {
+            setX(currentX);
+            setY(currentY);
+            roundRect.set(0, 0, currentW, sizeH);
+            canvas.drawRoundRect(roundRect, radius, radius, zonePaint);
+            canvas.drawText(text, startX + paddingSize, baseLineY, textPaint);
+            canvas.drawCircle(currentW - (sizeH / 2), sizeH / 2, radius, zonePaint);
+            canvas.drawBitmap(bitmapDisplay, currentW - sizeH + (sizeH - bitmapW) / 2, (sizeH - bitmapH) / 2, zonePaint);
+        } else {
+            canvas.drawCircle(currentW - (sizeH / 2), sizeH / 2, radius, zonePaint);
+            canvas.drawBitmap(bitmapDisplay, currentW - sizeH + (sizeH - bitmapW) / 2, (sizeH - bitmapH) / 2, zonePaint);
+        }
+    }
+
+    /**
+     * 只有在view绘制完成过后才能获取x,y,left,right
+     *
+     * @param hasWindowFocus
+     */
+    @Override
+    public void onWindowFocusChanged(boolean hasWindowFocus) {
+        super.onWindowFocusChanged(hasWindowFocus);
+        ValueAnimator animatorX = ValueAnimator.ofFloat(startX, endX);
+        ValueAnimator animatorY = ValueAnimator.ofFloat(startY, endY);
+        ValueAnimator animatorW = ValueAnimator.ofFloat(startSizeW, endSizeW);
+        ValueAnimator animatorRotate = ValueAnimator.ofFloat(0f, 360f);
+        animatorRotate.setDuration(5000);
+        animatorRotate.setInterpolator(new LinearInterpolator());//线性渐变
+        animatorRotate.setRepeatCount(ObjectAnimator.INFINITE);//无限循环
+        animatorRotate.setRepeatMode(ObjectAnimator.RESTART);
+        animatorX.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                currentX = (float) valueAnimator.getAnimatedValue();
+                requestLayout();
+                invalidate();
+            }
+        });
+        animatorY.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                currentY = (Float) valueAnimator.getAnimatedValue();
+            }
+        });
+
+        animatorW.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                currentW = (float) valueAnimator.getAnimatedValue();
+            }
+        });
+        animatorRotate.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                matrix.setRotate((Float) valueAnimator.getAnimatedValue());
+                bitmapDisplay = Bitmap.createBitmap(bitmap, 0, 0, bitmapW, bitmapH, matrix, true);
+                invalidate();
+            }
+        });
+
+        set = new AnimatorSet();
+        set.playTogether(animatorX, animatorY, animatorW);
+        set.setDuration(during);
+        set.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+//                transComplete = true;
+//                animatorRotate.start();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
+    }
+}
